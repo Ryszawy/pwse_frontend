a0e7f9d7e23cdf8c9735ac023ae6dd0babd8e034
Ryszawy <Krafi604@gmail.com>
2023-04-27 01:16:02 +0200

    svg icon in header added

--- src/components/MainHeader/MainHeader.jsx
+++ src/components/MainHeader/MainHeader.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import Navigation from './Navigation';
+import FlicktrendzIcon from '../UI/FlicktrendzIcon';
+
 import classes from './MainHeader.module.css';
 
 const MainHeader = () => {
   return (
     <header className={classes['main-header']}>
-      <h1>Flicktrendz</h1>
+      <div className={classes.icon}>
+        <FlicktrendzIcon />
+        <h1>licktrendz</h1>
+      </div>
       <Navigation />
     </header>
   );
